eb3bc7aa8fc470af613b1ffbb3eb61bbd9d0fe05
Nathan Michlo <nathan@dragonfruit.ai>
2025-01-21 05:30:25 +0200

    new test file

---
+++ tests/test-packages/D.py
@@ -0,0 +1,21 @@
+import B
+import B.b1
+import B.b2 as b2
+import extern_D
+import extern_D.sub_extern_D
+import extern_D.sub_extern_D_ as sub_extern_D_
+
+# could be a module
+from A import a1
+
+# a function
+from A.a2 import lazy
+
+# could be a module
+from A.a3 import a3i
+
+# could be a module
+from extern_D import sub_extern_D2
+
+# could be a module
+from extern_D.sub_extern_D3 import asdf
